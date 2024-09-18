57206c0c70f208977402489ff425c055e17f204f
yadsendew <44693107+yadsendew@users.noreply.github.com>
2024-09-18 13:39:56 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,37 @@
+async function createApeToken(imageBase64, imageType, chainId, creator, name, symbol, description, telegram, twitter, website) {
+  const formData = new FormData();
+  const byteCharacters = atob(imageBase64);
+  const byteNumbers = new Array(byteCharacters.length);
+  for (let i = 0; i < byteCharacters.length; i++) {
+    byteNumbers[i] = byteCharacters.charCodeAt(i);
+  }
+  const byteArray = new Uint8Array(byteNumbers);
+  const blob = new Blob([byteArray], { type: imageType });
+  formData.append('files', blob, 'image');
+  formData.append('data.Chain', `${chainId}`);
+  formData.append('data.Creator', creator);
+  formData.append('data.Name', name);
+  formData.append('data.Symbol', symbol);
+  formData.append('data.Description', description);
+  formData.append('data.Telegram', telegram);
+  formData.append('data.Twitter', twitter);
+  formData.append('data.Website', website);
+
+  try {
+    const response = await fetch('https://ape.store/api/token', {
+      method: 'POST',
+      body: formData,
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    console.log('Success:', result);
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+window.createApeToken = createApeToken;
