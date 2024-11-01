a08eaf654c846dcfb5e1b8f3e121ce09f7ff9326
yadsendew <44693107+yadsendew@users.noreply.github.com>
2024-11-01 20:35:07 +0700

    Update mpLoadTokens.js

--- mpLoadTokens.js
+++ mpLoadTokens.js
@@ -2,20 +2,24 @@
 async function mpLoadTokens(pageNumber) {
   try {
     const response = await fetch(`https://app.geckoterminal.com/api/p1/tags/pump-fun/pools?page=${pageNumber}&include=dex.network%2Ctokens&volume_24h%5Bgte%5D=30000&pool_creation_hours_ago%5Blte%5D=12`, {
-        "cache": "default",
-        "credentials": "omit",
-        "headers": {
-            "Accept": "application/json, text/plain, */*",
-            "Accept-Language": "en-US,en;q=0.9",
-            "Cache-Control": "no-cache",
-            "Pragma": "no-cache",
-            "Priority": "u=3, i"
-        },
-        "method": "GET",
-        "mode": "cors",
-        "redirect": "follow",
-        "referrer": "https://www.geckoterminal.com/",
-        "referrerPolicy": "strict-origin-when-cross-origin"
+      "headers": {
+        "accept": "application/json, text/plain, */*",
+        "accept-language": "en-US,en;q=0.9",
+        "if-none-match": "W/\"ba0d9a51a476c533651215d5093d022e\"",
+        "priority": "u=1, i",
+        "sec-ch-ua": "\"Not;A=Brand\";v=\"24\", \"Chromium\";v=\"128\"",
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": "\"macOS\"",
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-site"
+      },
+      "referrer": "https://www.geckoterminal.com/",
+      "referrerPolicy": "strict-origin-when-cross-origin",
+      "body": null,
+      "method": "GET",
+      "mode": "cors",
+      "credentials": "omit"
     });
 
     if (!response.ok) {
